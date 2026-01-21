2283608002de6d145004eac969e9935bee1848fc
PeerapornS <peerapornsuk@gosoft.co.th>
2026-01-21 13:39:05 +0700

    add github pages deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,35 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+    // createBrowserRouter, // For Vercel
+    createHashRouter, // For GitHub Pages
+    RouterProvider,
+} from 'react-router-dom';
 // import global style
 import './styles/globals.css';
 // import pages
 import PomodoroPage from './pages/PomodoroPage';
 import { PomodoroProvider } from './features/pomodoro/contexts/PomodoroContext';
 
-const router = createBrowserRouter([
+/* For Vercel */
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <PomodoroPage />,
+//     },
+//     // Add more routes here:
+//     // {
+//     //     path: '/settings',
+//     //     element: <SettingsPage />,
+//     // },
+// ]);
+
+/* For GitHub Pages */
+const router = createHashRouter([
     {
         path: '/',
         element: <PomodoroPage />,
     },
-    // Add more routes here:
-    // {
-    //     path: '/settings',
-    //     element: <SettingsPage />,
-    // },
 ]);
 
 createRoot(document.getElementById('root')!).render(
